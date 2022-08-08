1d0247901aa2997e0ebb35ddc94955c9250a47f8
vivianchanwh <vivianchanwenghei@gmail.com>
2022-08-08 12:16:07 +0100

    Tested driver scripts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FYP-Viv-Sxw
-In our capstone project, several robots using Raspberry Pi controllers are programmed to demonstrate swarm behaviours.
+In our capstone project (FYP), several swarm robots using Raspberry Pi controllers are programmed to demonstrate swarm behaviours.
+
+ 
---
+++ Servo_Driver_HAT/test-class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import time
+import math
+import smbus
+
+import sys
+sys.path.append("/home/pi/FYP-Viv-Sxw/Servo_Driver_HAT")
+import PCA9685
+
+if __name__=='__main__':
+
+  pwm = PCA9685.PCA9685(0x40, debug=False)
+  pwm.setPWMFreq(50)
+  while True:
+   # setServoPulse(2,2500)
+    for i in range(500,2500,10):
+      pwm.setServoPulse(3,i)
+      time.sleep(0.02)
+
+    for i in range(2500,500,-10):
+      pwm.setServoPulse(3,i)
+      time.sleep(0.02)
+
+
