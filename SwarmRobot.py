f932c12d9150ee502746b15348847ab873888d5a
unknown <sohxuanwei88@gmail.com>
2022-08-11 17:18:47 +0800

    cleaned up SwarmRobot.py

--- SwarmRobot.py
+++ SwarmRobot.py
@@ -12,6 +12,8 @@ class SwarmRobot:
   LEFT_WHEEL           = 15     #port 3 = Left wheel
   RIGHT_WHEEL          = 3      #port 15 = right wheel
   WHEEL_DIAMETER       = 66.5   #diameter of wheel = 66.5 mm
+  MOVE_SPEED           = 10     #movement speed of the robot, in centimeters/second
+  ANGULAR_SPEED        = 90     #angular speed of the robot, in degrees/second
 
   def __init__(self):
     #setup servo by creating a PCA9685 instance
@@ -25,77 +27,39 @@ class SwarmRobot:
     self.servo.setServoPulse(self.RIGHT_WHEEL, 0)
     time.sleep(1)
 
-  def forwardCycle(self, c_range=1):
-    # movement based on the sample code, 0 to 180 degrees
-    # c_range=1 represents 180 degrees cycle, <1 represents a partial cycle
-    if c_range == 0:
-      c_range = 0.1     #minimum value of c_range
-      
-    cycle_limit = 500 + 2000*c_range
-    for i in range(500, cycle_limit, 10):
-      self.servo.setServoPulse(self.LEFT_WHEEL, i)
-      self.servo.setServoPulse(self.RIGHT_WHEEL, i)
-      time.sleep(0.02)
-
-  def backwardCycle(self, c_range=1):
-    # movement based on the sample code, 0 to 180 degrees
-    # c_range=1 represents 180 degrees cycle, <1 represents a partial cycle
-    if c_range == 0:
-      c_range = 0.1     #minimum value of c_range
-      
-    cycle_limit = 500 + 2000*c_range
-    for i in range(cycle_limit, 500, -10):
-      self.servo.setServoPulse(self.LEFT_WHEEL, i)
-      self.servo.setServoPulse(self.RIGHT_WHEEL, i)
-      time.sleep(0.02)
-
   def moveForward(self, dist, backwards=False):
-    #based on distance (in meters), calculate number of cycles needed
-    cycles = round(((dist*1000) / self.WHEEL_DIAMETER), 1) #*1000 converts distance from meters to millimeters
-    #*2 to compensate for 180 degrees rotation instead of 360 degrees
-    cycles = cycles*2
-   
     if backwards == False:
-      for i in range(int(cycles)): 
-        self.forwardCycle(1)
-      self.forwardCycle(cycles - math.floor(cycles))
-    else:
-      for i in range(int(cycles)): 
-        self.backwardCycle(1)
-      self.backwardCycle(cycles - math.floor(cycles))
+      self.servo.setServoPulse(self.LEFT_WHEEL, 2500)
+      self.servo.setServoPulse(self.RIGHT_WHEEL, 500)
+    else:  
+      self.servo.setServoPulse(self.LEFT_WHEEL, 500)
+      self.servo.setServoPulse(self.RIGHT_WHEEL, 2500)
+      
+    #calculations of the travel time, tt
+    tt = round(dist / MOVE_SPEED, 1)
+    print("[92m[m][0m Moving " + round(dist,1) + " meters  in " + tt + " seconds")
+    
+    #constant speed for tt seconds, until servo stops
+    self.stopServo(tt)
+
+  def rotateSelf(self, rot, clockwise=False):
+    #perform clockwise/anticlockwise rotation
+    if clockwise == False:
+      self.servo.setServoPulse(self.LEFT_WHEEL, 500)
+      self.servo.setServoPulse(self.RIGHT_WHEEL, 500)
+    else: 
+      self.servo.setServoPulse(self.LEFT_WHEEL, 2500)
+      self.servo.setServoPulse(self.RIGHT_WHEEL, 2500)
+      
+    #calculation of rotation time, rt
+    rt = round(rot / ANGULAR_SPEED, 1)
+    print("[93m[m][0m Rotating " + round(rot,1) + " degrees  in " + rt + " seconds")
+    
+    self.stopServo(rt)
 
   def tryMoveForward(self):
-    for i in range(500, 2500, 10):
-      self.servo.setServoPulse(self.LEFT_WHEEL, i)
-      self.servo.setServoPulse(self.RIGHT_WHEEL, 3000-i)
-      time.sleep(0.01)
-    
-    #constanst speed then stops the servo 
-    self.stopServo(2)
+    moveForward(20, False)
 
-  def nothing(self):
-    for i in range(300, 100, -20):
-      self.servo.setServoPulse(self.LEFT_WHEEL, 1550-i)
-      self.servo.setServoPulse(self.RIGHT_WHEEL, 1550+i)
-      time.sleep(0.01)
-    
-    #pause here
-    self.stopServo(1)
-    time.sleep(1)
-
-    for i in range(50, 300, 10):
-      self.servo.setServoPulse(self.RIGHT_WHEEL, 1550-i)
-      self.servo.setServoPulse(self.LEFT_WHEEL, 1550+i)
-      time.sleep(0.01)
-
-    #constant speed
-    time.sleep(0.5)
-
-    for i in range(300, 100, -20):
-      self.servo.setServoPulse(self.RIGHT_WHEEL, 1550-i)
-      self.servo.setServoPulse(self.LEFT_WHEEL, 1550+i)
-      time.sleep(0.01)
-    self.stopServo(1)
-    time.sleep(1)
+ 
 
 
---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#initialisation script, run this after cloning the GitHub repo
+
+sudo echo 'adding custom aliases' > ~/.bashrc
+sudo echo 'alias fyp="cd /home/pi/FYP-Viv-Sxw"' > ~/.bashrc
+sudo echo 'alias test="./tesh.sh"' > ~/.bashrc
